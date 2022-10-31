31beb2a51cd8eafa1f955d0a5549017d102ad546
Jared Duke <jdduke@google.com>
2022-11-01 01:50:04 +0900

    peridot: Enable R8 optimizations for system_server and SystemUI
    Opt in to whole-program Java optimizations via R8 for Pixel devices.
    This can be explicitly disabled during builds via
    `SYSTEMUI_OPTIMIZE_JAVA=false SYSTEM_OPTIMIZE_JAVA=false m`.
    
    Note that this change does introduce changes to stack traces that
    may require retracing for fully accurate debugging and diagonstics.
    See go/aperf-system-r8-faq for additional details and guidance. Also
    note that optimizations are implicitly disabled in ENG builds.
    
    Results:
     * SystemUIGoogle.apk (40MB -> 35MB)
     * SystemUIGoogle.odex (32MB -> 25MB)
     * services.jar (20MB -> 17MB)
    
    Test: m + atest SystemUITests + presubmit + manual testing on Raven
    Test: m + atest FrameworksServicesTests + manual testing on Raven
    Bug: 203472868
    Change-Id: Id3766d8279a5b480e374427f9a5994c9b444640e

--- device.mk
+++ device.mk
@@ -170,8 +170,8 @@ PRODUCT_PACKAGES += \
 
 # Enable whole-program R8 Java optimizations for SystemUI and system_server,
 # but also allow explicit overriding for testing and development.
-SYSTEM_OPTIMIZE_JAVA = true
-SYSTEMUI_OPTIMIZE_JAVA = true
+SYSTEM_OPTIMIZE_JAVA := true
+SYSTEMUI_OPTIMIZE_JAVA := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \

5a5b137ef839189e7d06a687f92bbaa5476439c4
ryznstk <agsmhrd1k4@gmail.com>
2026-04-03 16:40:33 +0700

    Reapply "peridot: Override HDR types for dolby vision"
    This reverts commit 3c9b071956a25e1af271da7a47f07a696182cecd.

--- device.mk
+++ device.mk
@@ -327,6 +327,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     NcmTetheringOverlay
 
+# Parts
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 

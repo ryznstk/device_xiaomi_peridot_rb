f0ea027e38fd441f879d7e1ca6c17d3f78e8690d
ryznstk <agusmhrd1k4@gmail.com>
2026-06-26 22:57:06 +0700

    Add hardware/google/pixel to soong_namespace and PRODUCT_SOONG_NAMESPACES

--- device.mk
+++ device.mk
@@ -376,6 +376,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     hardware/google/interfaces \
+    hardware/google/pixel \
     hardware/google/pixel/pixelstats \
     hardware/google/pixel/power-libperfmgr \
     hardware/lineage/interfaces/power-libperfmgr \

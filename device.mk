545a867f02be37f59ab609d494e989c5c725125e
Khargosxh18 <nikhilyadav31071@gmail.com>
2026-08-24 17:06:26 +0000

    peridot: configs: provide own task_profiles.json

--- device.mk
+++ device.mk
@@ -456,7 +456,7 @@ PRODUCT_COPY_FILES += \
 
 # Task Profiles
 PRODUCT_COPY_FILES += \
-    system/core/libprocessgroup/profiles/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
+    $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json \
     system/core/libprocessgroup/profiles/cgroups.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json
 
 # Telephony

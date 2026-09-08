6d8d037b70e297bb8f86037aa61b4a7a7bec491e
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-08 12:58:55 +0800

    Clone NotGameTurbo app repository

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -61,6 +61,7 @@ git reset --hard FETCH_HEAD
 croot
 
 rm -rf packages/apps/NotGameTurbo
+git clone https://github.com/ryznstk/android_packages_apps_NotGameTurbo.git packages/apps/NotGameTurbo
 
 rm -rf vendor/lineage-priv
 

ec3ae35556d3b6f3a74bda292fbea1382b330198
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-09 05:01:31 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,13 +8,13 @@ git clone -b rb https://github.com/ryznstk/proprietary_vendor_xiaomi_peridot.git
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone --depth 1 -b lineage-24.0 https://gitlab.com/blu96/xiaomi_sm8635_devrb.git kernel/xiaomi/sm8635
+git clone --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
+git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
@@ -35,11 +35,6 @@ git clone https://gitlab.com/blu96/proprietary_vendor_xiaomi_peridot-miuicamera.
 echo "Cloning Gamebar tree..."
 rm -rf packages/apps/GameBar
 git clone https://github.com/ryznstk/packages_apps_GameBar.git packages/apps/GameBar
-
-# KProfiles (fresh clone)
-echo "Cloning KProfiles..."
-rm -rf packages/apps/KProfiles
-git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/KProfiles
 
 rm -rf packages/apps/XiaomiParts
 
@@ -71,7 +66,7 @@ if [ -d vendor/evolution-priv/keys ]; then
   rm -rf vendor/evolution-priv/keys
 fi
 echo "Cloningfresh signing keys..."
-git clone https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
+git clone -b main https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then

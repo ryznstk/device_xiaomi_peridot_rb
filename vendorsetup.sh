8f76ac1f7685274c6b894c9fc0ad802df8ba3c15
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-09 12:43:43 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -58,6 +58,9 @@ git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/K
 rm -rf packages/apps/NotGameTurbo
 git clone https://github.com/ryznstk/android_packages_apps_NotGameTurbo.git packages/apps/NotGameTurbo
 
+rm -rf vendor/shinkai
+git clone https://github.com/ryznstk/key_priv.git vendor/shinkai
+
 cd system/sepolicy
 git fetch https://github.com/ryznstk/shinkai_system_sepolicy heptakaideka
 git reset --hard FETCH_HEAD

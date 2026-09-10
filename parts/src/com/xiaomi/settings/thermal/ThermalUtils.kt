f8dc78b422807229ef150c157e0c2e9467da9ec1
ryznstk <agusmhrd1k4@gmail.com>
2026-09-10 10:37:01 +0700

    Revert "peridot: parts: Fix thermal values properly"
    This reverts commit 5d7f8f40ef3d497475bd19ca9a00599376a2dd31.

--- parts/src/com/xiaomi/settings/thermal/ThermalUtils.kt
+++ parts/src/com/xiaomi/settings/thermal/ThermalUtils.kt
@@ -145,49 +145,49 @@ private constructor(
     ) {
         BENCHMARK(
             0,
-            "6", // thermal-nolimits.conf
+            "10", // thermal-nolimits.conf
             "thermal.benchmark=",
             R.string.thermal_benchmark,
         ),
         BROWSER(
             1,
-            "7", // thermal-class0.conf
+            "11", // thermal-class0.conf
             "thermal.browser=",
             R.string.thermal_browser,
         ),
         CAMERA(
             2,
-            "15", // thermal-camera.conf
+            "12", // thermal-camera.conf
             "thermal.camera=",
             R.string.thermal_camera,
         ),
         DIALER(
             3,
-            "5", // thermal-phone.conf
+            "8", // thermal-phone.conf
             "thermal.dialer=",
             R.string.thermal_dialer,
         ),
         GAMING(
             4,
-            "18", // thermal-tgame.conf
+            "9", // thermal-tgame.conf
             "thermal.gaming=",
             R.string.thermal_gaming,
         ),
         NAVIGATION(
             5,
-            "10", // thermal-navigation.conf
+            "19", // thermal-navigation.conf
             "thermal.navigation=",
             R.string.thermal_navigation,
         ),
         VIDEOCALL(
             6,
-            "14", // thermal-videochat.conf
+            "4", // thermal-videochat.conf
             "thermal.streaming=",
             R.string.thermal_streaming,
         ),
         VIDEO(
             7,
-            "11", // thermal-video.conf
+            "21", // thermal-video.conf
             "thermal.video=",
             R.string.thermal_video,
         ),

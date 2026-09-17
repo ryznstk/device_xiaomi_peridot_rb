6c8a080dc51add0fdbf94345047f763b645c6eba
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-17 08:47:18 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -66,14 +66,6 @@ git clone https://github.com/ryznstk/packages_apps_KProfiles.git packages/apps/K
 rm -rf packages/apps/NotGameTurbo
 git clone https://github.com/ryznstk/android_packages_apps_NotGameTurbo.git packages/apps/NotGameTurbo
 
-rm -rf vendor/custom
-git clone https://gitlab.com/blu96/key_priv.git vendor/custom
-
-cd system/sepolicy
-git fetch https://github.com/ryznstk/shinkai_system_sepolicy heptakaideka
-git reset --hard FETCH_HEAD
-croot
-
 # Refresh signing keys
 if [ -d vendor/lineage-priv/keys ]; then
   echo "Removing existing signing keys..."

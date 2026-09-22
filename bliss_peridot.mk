e1f1ac19990031f83b76e6311b695a1e1f0ba6af
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-23 04:42:21 +0700

    Update and rename lineage_peridot.mk to bliss_peridot.mk

--- bliss_peridot.mk
+++ bliss_peridot.mk
@@ -9,7 +9,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from peridot device
 $(call inherit-product, device/xiaomi/peridot/device.mk)

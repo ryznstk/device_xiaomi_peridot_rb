d6b1262b40b6e871f316bf9a5be192220a6f4128
BLU.rzyn <agusmhrd1k4@gmail.com>
2026-09-22 16:42:20 +0700

    Update kernel and hardware source repository URLs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,26 +8,18 @@ git clone -b rb https://github.com/ryznstk/proprietary_vendor_xiaomi_peridot.git
 # Kernel source (fresh clone)
 echo "Cloning kernel source tree..."
 rm -rf kernel/xiaomi/sm8635
-git clone --depth 1 -b lineage-24.0 https://gitlab.com/blu96/xiaomi_sm8635_devrb.git kernel/xiaomi/sm8635
+git clone --depth 1 https://github.com/ryznstk/android_kernel_xiaomi_sm8635.git kernel/xiaomi/sm8635
 
 rm -rf kernel/xiaomi/sm8635-modules
-git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
+git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-modules.git kernel/xiaomi/sm8635-modules
 
 rm -rf kernel/xiaomi/sm8635-devicetrees
-git clone -b lineage-23.2 --depth 1 https://github.com/ryznstk/xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
-
-cd kernel/xiaomi/sm8635
-curl https://raw.githubusercontent.com/maxsteeel/nomount/refs/heads/dev/kernel/setup.sh | bash -
-croot
-
-cd kernel/xiaomi/sm8635
-curl -LSs "https://raw.githubusercontent.com/ReSukiSU/ReSukiSU/main/kernel/setup.sh" | bash
-croot
+git clone -b lineage-23.2 --depth 1 https://github.com/LineageOS/android_kernel_xiaomi_sm8635-devicetrees.git kernel/xiaomi/sm8635-devicetrees
 
 # Hardware xiaomi (fresh clone)
 echo "Cloning hardware xiaomi source..."
 rm -rf hardware/xiaomi
-git clone -b los https://github.com/ryznstk/hardware_xiaomi_los.git hardware/xiaomi
+git clone -b ll https://github.com/ryznstk/hardware_xiaomi_los.git hardware/xiaomi
 
 # MiuiCamera device tree (fresh clone)
 echo "Cloning MiuiCamera device tree..."
@@ -69,6 +61,10 @@ git clone https://github.com/AxionAOSP-devices/android_packages_apps_KProfiles.g
 rm -rf packages/apps/NotGameTurbo
 git clone https://github.com/ryznstk/android_packages_apps_NotGameTurbo.git packages/apps/NotGameTurbo
 
+rm -rf packages/apps/LunarisDolby
+rm -rf packages/apps/DolbyAtmos
+git clone https://github.com/unmoved21/packages_apps_LunarisDolby.git packages/apps/LunarisDolby
+
 rm -rf vendor/lineage-priv
 
 # Refresh signing keys
@@ -77,7 +73,7 @@ if [ -d vendor/evolution-priv/keys ]; then
   rm -rf vendor/evolution-priv/keys
 fi
 echo "Cloningfresh signing keys..."
-git clone -b main https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
+git clone -b evo https://github.com/ryznstk/keys.git vendor/evolution-priv/keys
 
 # Always back to root at the end
 if command -v croot &>/dev/null; then
